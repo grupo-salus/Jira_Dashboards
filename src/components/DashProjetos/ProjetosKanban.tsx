86e7edda61c3e8dbd6be06a19728c185c0502cda
Luis Gomes <luis.gomes_@outlook.com>
2025-06-23 11:48:52 -0300

    feat: Atualiza a cor de fundo dos cartões do Kanban para um tom mais escuro, melhorando a estética e a legibilidade no modo escuro do dashboard.

--- src/components/DashProjetos/ProjetosKanban.tsx
+++ src/components/DashProjetos/ProjetosKanban.tsx
@@ -53,7 +53,7 @@ const KanbanCard: React.FC<{ projeto: EspacoDeProjetos }> = ({ projeto }) => {
   return (
     // essa div é o card do kanban
     <div
-      className="group relative flex w-full bg-white dark:bg-gray-900 rounded-lg p-2 shadow-sm border border-gray-200 dark:border-gray-700 hover:bg-white/90 dark:hover:bg-gray-900/90 transition-shadow cursor-pointer items-start"
+      className="group relative flex w-full bg-white dark:bg-[#151f2e] rounded-lg p-2 shadow-sm border border-gray-200 dark:border-gray-700 hover:bg-white/90 dark:hover:bg-gray-900/90 transition-shadow cursor-pointer items-start"
       tabIndex={0}
     >
       {/* Barra de prioridade */}
